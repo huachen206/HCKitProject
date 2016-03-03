29dac1a70b1835652caff06dcdc29bacc644b925
huachen <huachen206@163.com>
2016-03-03 09:29:17 +0800

    update

---
+++ HCDB/NSObject+HCExtend.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+HCExtend.h
+//  HCKitProject
+//
+//  Created by HuaChen on 16/2/15.
+//  Copyright © 2016年 花晨. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (HCExtend)
+
+@end
---
+++ HCDB/NSObject+HCExtend.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+HCExtend.m
+//  HCKitProject
+//
+//  Created by HuaChen on 16/2/15.
+//  Copyright © 2016年 花晨. All rights reserved.
+//
+
+#import "NSObject+HCExtend.h"
+
+@implementation NSObject (HCExtend)
+
+@end
